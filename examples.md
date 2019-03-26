430398b8527fb18c680724c29a8c81c19e3ff2e8
Andreas Krueger <coding@AndreasKrueger.de>
2019-03-26 21:55:39 +0100

    v0.1 is working

---
+++ examples.md
@@ -0,0 +1,59 @@
+# Example outputs
+### Example when no parameters are given:
+```
+python3 redduser.py
+Arguments given: (days=2, file='comments.json', user='andreaskrueger')
+Downloading until I see a post which was before '20190324-201225'
+
+https://www.reddit.com/user/andreaskrueger/comments/.json?limit=4&after=None <Response [200]>
+20190326-125545  +3 r/ABoringDys reddit.com/comments/b5ibwu/-/ejew7v4  &gt; you have to raise $1,000  He couldn
+20190325-230111  +1 r/cscareerqu reddit.com/comments/b59yw6/-/ejdjs57  Cannot imagine not having learned it.
+20190325-221822  +4 r/collapse   reddit.com/comments/b5bjym/-/ejdg3zl  20-30 years too late. That's why "democr
+20190325-060231  +1 r/entertainm reddit.com/comments/b4njal/-/ejblaiu  The **500x is pure propaganda**. It's in
+
+https://www.reddit.com/user/andreaskrueger/comments/.json?limit=4&after=t1_ejblaiu <Response [200]>
+20190325-051950  +1 r/entertainm reddit.com/comments/b4njal/-/ejbjatf  Agreed, but **the 500x is pure propagand
+20190325-040452  +1 r/collapse   reddit.com/comments/b4yb7e/-/ejbf79i  Hierarchy.
+20190325-040014  +2 r/collapse   reddit.com/comments/b4yb7e/-/ejbex6q  &gt; locked collars on their security te
+20190325-035208  +1 r/WhitePeopl reddit.com/comments/b50mmx/-/ejbef2g  And now [they build doomsday bunkers for
+
+https://www.reddit.com/user/andreaskrueger/comments/.json?limit=4&after=t1_ejbef2g <Response [200]>
+20190325-034955  +6 r/collapse   reddit.com/comments/b4yb7e/-/ejbea7t  &gt; intervene  Oh, intervened they have
+20190325-033504  +1 r/WhitePeopl reddit.com/comments/b50mmx/-/ejbdc00  Plus:  - a disconnected caste which live
+20190324-181714  +1 r/DataHoarde reddit.com/comments/b35zfh/-/eja52s4  Thanks for the docs hint. I have found t
+20190324-045930  +1 r/Catastroph reddit.com/comments/b4r207/-/ej8x9lz  Buying cheap stuff on eBay and amazon.
+
+Oldest seen post was at '20190324-045930' which is more than 2 days ago. Breaking from the loop.
+
+Done. Written 12 comments to file 'comments.json' (32937 bytes).
+```
+
+### Help for parameters:
+```
+python3 redduser.py -h
+
+usage: redduser.py [-h] [--user USER] [--days DAYS] [--file FILE]
+
+Download recent reddit-comments of a user. Version v0.1.
+
+optional arguments:
+  -h, --help   show this help message and exit
+  --user USER  reddit username (default: andreaskrueger)
+  --days DAYS  go back how many days? (default: 2)
+  --file FILE  output file (default: comments.json)
+```
+
+### Example for a user with very few (2) total posts:
+```
+python3 redduser.py --user Stegoserious --days 1000 --file Stegoserious.json
+
+Arguments given: (days=1000, file='Stegoserious.json', user='Stegoserious')
+Downloading until I see a post which was before '20160629-202700' UTC.
+
+https://www.reddit.com/user/Stegoserious/comments/.json?limit=4&after=None <Response [200]>
+20190326-145851    +28 r/bestof     reddit.com/comments/b5k9jr/-/ejf5zlq  Yes, this is my actual first post.  No, 
+20190325-223716 +10843 r/dataisbeau reddit.com/comments/b5f9wi/-/ejdhqf8  I'm probably in the top 5% of longest lu
+Returned pagination parameter 'after' was 'None' = No more posts! Breaking from the loop.
+
+Done. Written 2 comments to file 'Stegoserious.json' (6176 bytes).
+```
---
+++ redduser.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+"""
+@summary: download all my reddit comments - from now, back 'days' into the past           
+@since:   26/March/2019
+@author:  https://github.com/drandreaskrueger
+@see:     https://github.com/drandreaskrueger/redduser for updates
+"""
+
+VERSION = "v0.1"
+
+################################################################################
+## Dependencies and defaults
+
+import os
+import time
+import requests
+import json
+import argparse
+from pprint import pprint
+
+DEFAULTS = {"days"       : 2,  # how many days into the past?
+            "filename"   : "comments.json", # output file
+            "user"       : "andreaskrueger", # me, I have written this for myself, lol
+            "bodyhead"   : 40, # print first x character of each post in statusline (0 for None)
+            "sr_name_len": 12, # statusline: cut subreddit name after x characters  
+            "limit"      : 4 } # comments per pagination - low for testing, high for production, max 100
+assert DEFAULTS["limit"]<=100
+
+HEADERS = {'User-agent': 'redduser v0.1'} # otherwise <Response Error: 429> 
+SHORTLINK_COMMENT = "reddit.com/comments/{thread_id}/-/{comment_id}"
+# SHORTLINK_COMMENT = "redd.it/{thread_id}/{comment_id}" # ???
+API_URL="https://www.reddit.com/user/{user}/comments/.json?limit={limit}&after={after}"
+
+################################################################################
+## Code
+
+
+def cli_args():
+    """
+    python3 redduser.py --help 
+    """
+    parser = argparse.ArgumentParser(description='Download recent reddit-comments of a user. Version %s.' % VERSION,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--user', help='reddit username', default=DEFAULTS["user"],)
+    parser.add_argument('--days', type=int, default=DEFAULTS["days"], help='go back how many days?')
+    parser.add_argument('--file', help='output file', default=DEFAULTS["filename"])
+    args = parser.parse_args()
+    print( ("Arguments given: %s" % args).replace("Namespace", ""))
+    return args
+
+
+def utc(epoch):
+    """
+    epochtime --> human readable UTC time
+    """
+    return time.strftime("%Y%m%d-%H%M%S", time.gmtime(epoch))
+# print (utc(epoch=1553604945.0)); exit()
+
+
+def print_comments(children):
+    """
+    given a reddit response, process the data:
+    print one child element at a time in a statusline
+    returns 
+    * the oldest appearing utc timestamp in this batch
+    """
+    datetimes=[] 
+    for c in children:
+        data=c["data"]
+        sl_params={"comment_id" : data["id"],
+                   "thread_id": data["link_id"][3:]} # get rid of the "t3_" prefix
+        shortlink=SHORTLINK_COMMENT.format(**sl_params)
+        body = data["body"] .replace('\r', ' ').replace('\n', ' ')
+        linedata = (utc(data["created_utc"]), data["score"], 
+                    data['subreddit_name_prefixed'][:DEFAULTS["sr_name_len"]], 
+                    shortlink, body[:DEFAULTS["bodyhead"]])
+        line = ("%s %+3d %-"+("%d"%DEFAULTS["sr_name_len"])+"s %s  %s") % linedata 
+        print (line)
+        datetimes.append(utc(data["created_utc"]))
+    oldest = min(datetimes) 
+    return oldest
+
+
+def iterate(params, days):
+    """
+    Collates all comments from now, back to 'days' ago;
+    infinite loop is only exited when an older timestamp is seen.
+    Loop repeatedly calls reddit API with each time an older 'after' parameter.
+    
+    Returns 
+    * would be next 'after' parameter for next batch (even further into the past)
+    * oldest timestamp contained
+    * all comments in one long list
+    """
+    comments = []
+    epochCutoff = time.time() - days * 24 * 60 * 60
+    dateCutoff = utc(epochCutoff)
+    print ("Downloading until I see a post which was before '%s' UTC." % dateCutoff)
+    oldest = None
+    
+    while True:
+        url=API_URL.format(**params)
+        print ("\n" + url, end=" ")
+        
+        try:
+            res = requests.get(url, headers=HEADERS)
+            r = res.json() # pprint(r)
+        except Exception as e:
+            print ("\nERROR: %s.%s\n %s" % (type(e).__module__, type(e).__name__, e))
+            exit()
+        print(res)
+        
+        # after = r["data"]["after"]
+        # if params["after"] == after:
+            # print ("Returned pagination parameter 'after' has not changed. No more posts? Breaking from the loop without even processing that last result.")
+            # break
+        
+        children=r["data"]["children"]
+        oldest = print_comments(children)
+        comments.extend(children)
+
+        after = r["data"]["after"]
+        if after == None:
+            print ("Returned pagination parameter 'after' was 'None' = No more posts! Breaking from the loop.")
+            break
+        params["after"] = after
+        
+        if oldest < dateCutoff:
+            print ("Oldest seen post was at '%s' which is more than %d days ago. Breaking from the loop." % (oldest, days))
+            break
+    
+    return after, oldest, comments
+
+
+def save_all(comments, filename):
+    """
+    writes list of all comments as JSON file
+    """
+    with open(filename, "w") as f:
+        json.dump(comments, f)
+    num=len(comments)
+    size = os.path.getsize(filename)
+    print("\nDone. Written %d comments to file '%s' (%d bytes)." % (num, filename, size))
+
+
+if __name__ == '__main__':
+    
+    args = cli_args()
+    
+    params={"user":args.user, 
+            "limit": DEFAULTS["limit"], 
+            "after": "None"}
+    _,_,comments = iterate(params, args.days)
+    
+    save_all(comments, args.file)
+    
